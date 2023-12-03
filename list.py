c028ae56e497420aa29805ead0a164a3da3f8fd9
MAHERoff <121495196+MAHERoff@users.noreply.github.com>
2023-12-03 10:29:17 +0000

    Add list.py : Use negatives indexes

--- list.py
+++ list.py
@@ -1,4 +1,3 @@
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
-planets.pop()  # Goodbye, Pluto
-number_of_planets = len(planets)
-print("No, there are definitely", number_of_planets, "planets in the solar system.")
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+print("The last planet is", planets[-1])
+print("The penultimate planet is", planets[-2])
