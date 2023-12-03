68ee4eacb413cdf7557db49b324fc4d7d648bd76
MAHERoff <121495196+MAHERoff@users.noreply.github.com>
2023-12-03 10:20:00 +0000

    Add list.py : Remove values ​​from lists

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-planets.append("Pluto")
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
+planets.pop()  # Goodbye, Pluto
 number_of_planets = len(planets)
-print("There are actually", number_of_planets, "planets in the solar system.")
+print("No, there are definitely", number_of_planets, "planets in the solar system.")
